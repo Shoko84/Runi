9d9b8405713c6618dc9eeb414bc51b2210e3fee0
Maxime Cauvin <maxime.cauvin@epitech.eu>
2019-05-18 23:01:50 +0900

    feat/develop: Initializing Unity project

---
+++ Runi/Assets/Scripts/RunaMainScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunaMainScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Yo I'm the big one");
+    }
+}
